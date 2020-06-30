89e0783a88f0d1888f3008a8867a3cd3c926e265
stefounet <s@fasterize.com>
2020-06-30 10:49:02 +0200

    Add usage with a TMS

---
+++ Queue/tc.js
@@ -0,0 +1,4 @@
+//define Queue as the first tag in TMS
+//Queue every tag or so with a priority
+//Unqueue at the end
+//Tags with the same priority will be launched at the same time after DELAY x PRIORITY 
